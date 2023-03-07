0fdf6305436d5868b2a222430cb228154922a676
ASKMEFORHACK <107302484+ASKMEFORHACK@users.noreply.github.com>
2023-03-07 19:41:08 +0000

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,35 @@
+// random value generated
+  var y = Math.floor(Math.random() * 10 + 1);
+  var x = document.getElementById("guessField").value;
+
+// counting the number of guesses
+function submit()
+{
+    if(x == y)
+{
+    alert("CONGRATULATIONS!!!"+playername+" You GUESSED IT RIGHT IN "
+    + guess + " GUESS ");
+    guess = 1;
+   
+    else if (x > y)
+    {
+        guess++;
+        alert("OOPS SORRY! TRY A SMALLER NUMBER");
+    }
+    
+    else
+    {
+        guess++;
+        alert("OOPS SORRY! TRY A GREATER NUMBER");
+    }
+}
+}
+
+// made for correct Guess
+
+ 
+function playagain(){
+    y = Math.floor(Math.random() * 10 + 1);
+}
+  
+// function for number guessed by user     
